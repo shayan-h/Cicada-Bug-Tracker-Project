a85a68163eaf91f7a2cabf519a08ce46b6507f91
shayan-h <shayanwork612@gmail.com>
2023-11-25 23:01:41 -0800

    Updated user's projects in db & new proj display on dashboard

--- routes/newProject.js
+++ routes/newProject.js
@@ -21,6 +21,7 @@ router.get('/', (req, res) => {
 })
 
 router.post('/', (req, res) => {
+  const email = req.user.email;
   const projectName = req.body.projectName
   const projectDescription = req.body.projectDescription
   let teamMembers = req.body.teamMembers || []
@@ -47,7 +48,32 @@ router.post('/', (req, res) => {
       }
       console.log('Project created succesfully');
     })
-    res.redirect('/dashboard')
+  
+  const query2 = "SELECT id FROM projects WHERE project_name = ?"
+  let project_id
+  connection.query(query2, [projectName],
+    (err, results) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('Project id fetched succesfully');
+      project_id = results[0].id;
+      console.log('Project ID: ', project_id)
+
+      const query3 = "UPDATE users SET projects = JSON_SET(COALESCE(projects, '{}'), '$.proj', '?') WHERE email = ?"
+      connection.query(query3, [project_id, email],
+        (err, results) => {
+          if (err) {
+          console.error(err); 
+          return;
+        }
+        console.log('Project added to user');
+      })
+      res.redirect('/dashboard')
+  })
+
+  
 })
 
 module.exports = router // exports router
