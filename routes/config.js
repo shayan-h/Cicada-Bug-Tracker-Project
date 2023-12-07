2047f35bd4938cc61bff036c78ad1e3dbf823455
shayan-h <shayanwork612@gmail.com>
2023-12-06 18:46:56 -0800

    Add config file

---
+++ routes/config.js
@@ -0,0 +1,12 @@
+// congig.js
+
+const config = {
+    database: {
+      host: 'ls-15bff70e52e9bac1fc53888b3db0bc81016d976a.cvwqtfwsr9av.us-west-2.rds.amazonaws.com',
+      user: 'dbcicadauser',
+      password: '0(ub}<n+vy;j3Np]e,p$n5q8ExB<2KQ)',
+      database: 'dbcicada'
+    }
+  };
+  
+  module.exports = config;
